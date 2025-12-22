dac1f8d0ffb4c0a4c87ded0ac36c299fc86baac9
Maxsimilian Amalathas <zlac425@live.rhul.ac.uk>
2025-12-22 21:14:50 +0000

    Fix Postgres connection drops with pool pre-ping

--- backend/database.py
+++ backend/database.py
@@ -7,6 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True, 
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+    )
+
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind=engine)
 Base = declarative_base()
